f152c030bffbc95f79b1a8b1a1f41be2084d088d
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2019-07-02 11:13:21 +0200

    Update ChickenRand.r

--- randomisation/ChickenRand.r
+++ randomisation/ChickenRand.r
@@ -1,13 +1,39 @@
 #Chicken randomisation function
-ChickenRand <- function(animalvector,animalexcludevector,totalgroupsize=6,sampledgroupsize=2,randomisations=4,batchpos=90,batchext=2,batchlib=2,batchpcr=2,fit=TRUE,seed=9999){
+#Author: Antton Alberdi
+#Date: 2019/07/02
+#Version: 1.0
 
+# FUNCTIONALITY
+# 1) Randomly select N number of animals from a pool of animals.
+# 2) Randomly distribute the selected animals into N number of plates. This step can be done in parallel for multiple batches, e.g. one per sample type.
+
+# ARGUMENTS
+# animalvector: vector of animal codes to be included in the randomisation
+# animalexcludevector: vector of animal codes to be excluded in the randomisation (e.g. samples randomised in a previous batch)
+# blocksize: number of animals in the lowest block size (pen). In the case of chicken: 6
+# blocksampled: number of animals in the lowest block size (pen) to be included in the randomisation. e.g. If 2, two chickens out of the six will be randomly selected per pen
+# batchnames: vector of batch names. e.g. c("F1","D1","B1") or c("IleumMucosa","CaecumMucosa","CaecumContent"). Samples will be randomly distributed into plates in each batch. 
+# positive: number of positive samples per plate. Default 90
+# negext: number of extraction controls per plate. Default 2
+# neglib: number of library controls per plate. Default 2
+# negpcr: number of indexing controls per plate. Default 2
+# fit: whether positive samples + controls will be fited to avoid semi-empty plates or not. Note that if TRUE, some of the selected samples will be removed from the plates.
+
+
+#Example: ChickenRand(animalvector=c("CB02.17","CA23.05","CB15.16","CB09.04","CB05.16","CA13.11"),animalexcludevector=c("CB02.17","CA23.05"),blocksize=6,blocksampled=2,batchnames=c('C1','F1','B1','D2'),positive=90,negext=2,neglib=2,negpcr=2,fit=TRUE,seed=112)
+
+
+
+ChickenRand <- function(animalvector,animalexcludevector,blocksize=6,blocksampled=2,batchnames,positive=90,negext=2,neglib=2,negpcr=2,fit=TRUE,seed=9999){
+
+#Exclude samples if argument animalexcludevector provided
 if(!missing(animalexcludevector)){
 animals <- setdiff(animalvector,animalexcludevector)
 }else{
 animals <- animalvector
 }
 
-batchsize <- batchpos + batchext + batchlib + batchpcr
+batchsize <- positive + negext + neglib + negpcr
 
 library(stringr)
 split.table <- as.data.frame(cbind(substring(animals, 1,2),substring(animals, 3,4),substring(animals, 6,7)))
@@ -24,9 +50,9 @@ rownames(split.table) <- c(1:nrow(split.table))
 
 #Randomly select animales per pen
 d <- c(1:nrow(split.table))
-d.split <- split(d, ceiling(seq_along(d)/totalgroupsize))
+d.split <- split(d, ceiling(seq_along(d)/blocksize))
 if(!missing(seed)){set.seed(seed)}
-d.split.subset <- lapply(d.split, function(x) sample(x,sampledgroupsize))
+d.split.subset <- lapply(d.split, function(x) sample(x,blocksampled))
 selected <- unlist(d.split.subset, use.names = FALSE)
 
 #Perform the subsetting
@@ -34,24 +60,24 @@ split.table.subset <- split.table[selected,]
 selected.animals <- paste(split.table.subset$trial, split.table.subset$pen,".",split.table.subset$animal, sep="")
 
 if(fit == TRUE){
-batchn = floor(length(selected.animals)/batchpos)
-samplen = batchn * batchpos
+batchn = floor(length(selected.animals)/positive)
+samplen = batchn * positive
 if(!missing(seed)){set.seed(seed)}
 selected.animals <- selected.animals[sample(1:length(selected.animals),samplen,replace = FALSE)]
 }
 
-#Iterate according to number of randomisations selected
+#Iterate according to number of randomisation batches selected
 rand.batches <- list(samples=selected.animals)
-for (r in c(2:(randomisations+1))){
+for (r in c(2:(length(batchnames)+1))){
 #Generate batches
 if(!missing(seed)){set.seed(seed)}
-batches <- split(sample(selected.animals), ceiling(seq_along(selected.animals)/batchpos))
+batches <- split(sample(selected.animals), ceiling(seq_along(selected.animals)/positive))
 names(batches) <- paste("Plate",c(1:length(batches)),sep="")
 
 #Add controls and randomise batches
-cont.ext <- paste("CE",c(1:batchext),sep="")
-cont.lib <- paste("CL",c(1:batchext),sep="")
-cont.pcr <- paste("CP",c(1:batchext),sep="")
+cont.ext <- paste("CE",c(1:negext),sep="")
+cont.lib <- paste("CL",c(1:negext),sep="")
+cont.pcr <- paste("CP",c(1:negext),sep="")
 batches.controls <- lapply(batches, function(x) {x <- c(x,cont.ext,cont.lib,cont.pcr)})
 if(!missing(seed)){set.seed(seed)}
 batches.controls.shuffled <- lapply(batches.controls, function(x) {x <- sample(x)})
@@ -62,7 +88,7 @@ batches.controls.shuffled[[length(batches.controls.shuffled)]] <- c(batches.cont
 }
 batches.controls.shuffled.matrix <- lapply(batches.controls.shuffled, function(x) {x <- matrix(x,nrow = 8,ncol = 12, byrow = TRUE, dimnames = list(c("A", "B","C","D","E","F","G","H"),c(1:12)))})
 rand.batches[[r]] <- batches.controls.shuffled.matrix
-names(rand.batches)[r] <- paste("Rand",(r-1),sep="")
+names(rand.batches)[r] <- batchnames[(r-1)]
 }
 
 #Return
